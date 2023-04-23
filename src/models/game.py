1563b72294082939a8db184e910f60b2ac7c5d9d
Quynh <qtd.nguyen16@gmail.com>
2023-04-22 23:49:18 -0500

    Add basic alien and detect when player is hit by alien bullet

--- src/models/game.py
+++ src/models/game.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 
 class Game:
-    def __init__(self, color=BLUE_RGB, scale=3, thickness=0.02):
+    def __init__(self, color=BLUE_RGB, alien_color=RED_RGB, scale=3, thickness=0.02):
         self.initialized = False
         self.w = None
         self.h = None
@@ -15,11 +15,13 @@ class Game:
         self.color = color
         self.thickness = thickness
         self.trigger = False
+        self.alien_color=alien_color
 
         # scale up hand position so it doesn't need to move entire width of screen
         self.scale = scale 
         
         self.bullets = []
+        self.aliens = [Alien()]
 
     def update(self, frame, results):
         if not self.initialized:
@@ -44,14 +46,48 @@ class Game:
             bullet.update() # update bullet position 
         
         return frame
+    
+    def draw_aliens(self, frame):
+        for alien in self.aliens:
+            x, y, health = alien.state()
+
+            if health <= 0:
+                self.aliens.remove(alien) # remove bullet if off screen
+
+            # draw alien
+            frame = cv2.circle(frame, (int(x * self.w), int(y * self.h)), int(0.05 * self.w), 
+                                  self.alien_color, thickness=-1) 
+            
+            for bullet in alien.bullets:
+                x, y = bullet.pos()
+
+                if x >= self.pos - self.len/2 and x <= self.pos + self.len/2 and y >= 1:
+                    print("YOU LOST")
+                    self.aliens.remove(alien)
+
+                if y >= 1:
+                    alien.bullets.remove(bullet) # remove bullet if off screen
+
+                # draw bullet
+                frame = cv2.rectangle(frame, (int(x * self.w), int(y * self.h)), 
+                                    (int((x+0.01) * self.w), int((y+0.05) * self.h)), 
+                                    self.alien_color, thickness=-1) 
+
+                bullet.update() # update bullet position 
+
+            if (alien.update(self.pos, self.len)):
+                print("YOU LOST") # update alien state
+                self.aliens.remove(alien)
+        
+        return frame
 
     def draw(self, frame):
         frame = cv2.rectangle(frame, (int(self.w * (self.pos - self.len / 2)), 
                                       int(self.h * (1 - self.thickness))), 
                                      (int(self.w * (self.pos + self.len / 2)), 
-                                      int(self.h)), 
-                                          self.color, thickness=-1)  
+                                      int(self.h)), self.color, thickness=-1)  
         frame = self.draw_bullets(frame)
+        frame = self.draw_aliens(frame)
         return frame
 
     def move(self, results):
@@ -80,14 +116,45 @@ class Game:
         self.bullets.append(Bullet(self.pos, 0.05))
         
 class Bullet:
-    def __init__(self, x, speed):
+    def __init__(self, x, speed, alien=False):
         self.x = x
         self.y = 1
         self.speed = speed
+        self.alien = alien
+
+        if alien:
+            self.y = 0
     
     def update(self):
-        self.y -= self.speed
+        if self.alien:
+            self.y += self.speed
+        else:
+            self.y -= self.speed
 
     def pos(self):
         return self.x, self.y
     
+class Alien:
+    def __init__(self, speed=50, bullet_speed=0.03):
+        self.health = 1
+        self.x = random.uniform(0, 1)
+        self.y = 0
+        self.bullets = []
+        self.count = 0
+        self.speed = speed
+        self.bullet_speed = bullet_speed
+
+    def state(self):
+        return self.x, self.y, self.health
+
+    def update(self, pos, len):
+        # shoot every self.speed frames
+        self.count += 1
+
+        if self.count == self.speed:
+            self.shoot()
+            self.count = 0
+
+    def shoot(self):
+        print("ALIEN: PEW!")
+        self.bullets.append(Bullet(self.x, self.bullet_speed, True))
